a6a842e3628c137e374830bf38933e980d9f3397
Drew Jex <drewjex@amazon.com>
2020-02-11 10:58:45 -0800

    initial commit for xstate

--- src/Page.js
+++ src/Page.js
@@ -1,68 +1,69 @@
 import React, { Component } from 'react';
-import PureIframe from "./PureIframe";
+import PureIframe from './PureIframe';
 
 export default class Page extends Component {
-    componentDidMount() {
-        const { color, index } = this.props;
-        this[`Page${index}`].style.background = color;
-        this.props.onLoad(index);
-    }
+  componentDidMount() {
+    const { color, index } = this.props;
+    this[`Page${index}`].style.background = color;
+    this.props.onLoad(index);
+  }
 
-    render() {
-        const { 
-            isSelected, 
-            isHiddenLeft, 
-            isHiddenRight, 
-            title, 
-            content, 
-            index, 
-            footer, 
-            header, 
-            iframe, 
-            image, 
-            imageStyle, 
-            isZoomed, 
-            style, 
-            onClick 
-        } = this.props; 
-        
-        return (
-            <div style={style} 
-                 className={`Page ${isSelected && 'selected'} ${isHiddenLeft && 'hiddenleft'} ${isHiddenRight && 'hiddenright'} ${header && 'has-header'} ${isZoomed && 'zoomed'}`}
-                 onClick={event => onClick(index)} 
-                 ref={node => (this[`Page${index}`] = node)}>
-                 {header && 
-                    <div className="header">
-                        {header}
-                    </div>
-                 }
-                <div className={`title ${index === 0 && 'big'}`}>
-                    {title}
-                </div>
-                <div className={`content ${index === 0 && 'big'}`}>
-                    {typeof content === "string" &&
-                        content
-                    }
-                    {content instanceof Array &&
-                        <ul>
-                            {content.map((c, i) => {
-                                return <li key={i}>{c}</li>
-                            })}
-                        </ul>
-                    }
-                    {iframe && 
-                        <PureIframe iframe={iframe} 
-                                    index={index} />
-                    }
-                    {image && 
-                        <img alt='' className={`${imageStyle === 'contain' && 'contain'}`} src={image} width="100%" height="100%" />
-                    }
-                </div>
-                <div className="footer">
-                    {footer ? footer : index}
-                </div>
-                <img alt='' className="logo" src="https://www.accessdevelopment.com/hs-fs/hubfs/AccessDevelopment_June2017/logo.png?t=1529951130710&width=138&name=logo.png" />
-            </div>
-        )
-    }
+  render() {
+    const {
+      isSelected,
+      isHiddenLeft,
+      isHiddenRight,
+      title,
+      content,
+      index,
+      footer,
+      header,
+      iframe,
+      image,
+      imageStyle,
+      isZoomed,
+      style,
+      onClick
+    } = this.props;
+
+    return (
+      <div
+        style={style}
+        className={`Page ${isSelected && 'selected'} ${isHiddenLeft &&
+          'hiddenleft'} ${isHiddenRight && 'hiddenright'} ${header &&
+          'has-header'} ${isZoomed && 'zoomed'}`}
+        onClick={event => onClick(index)}
+        ref={node => (this[`Page${index}`] = node)}
+      >
+        {header && <div className="header">{header}</div>}
+        <div className={`title ${index === 0 && 'big'}`}>{title}</div>
+        <div className={`content ${index === 0 && 'big'}`}>
+          {typeof content === 'string' && content}
+          {content instanceof Array && (
+            <ul>
+              {content.map((c, i) => {
+                return <li key={i}>{c}</li>;
+              })}
+            </ul>
+          )}
+          {iframe && <PureIframe iframe={iframe} index={index} />}
+          {image && (
+            <img
+              alt=""
+              className={`${imageStyle === 'contain' && 'contain'}`}
+              src={image}
+              width="100%"
+              height="100%"
+            />
+          )}
+        </div>
+        <div className="footer">{footer ? footer : index}</div>
+        <img
+          alt=""
+          className="logo"
+          src="https://banner2.kisspng.com/20180426/hoe/kisspng-amazon-com-amazon-web-services-cloud-computing-amazon-logo-5ae209d786e676.3280528715247630955526.jpg"
+        />
+      </div>
+    );
+  }
 }
